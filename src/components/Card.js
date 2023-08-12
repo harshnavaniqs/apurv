9797606a42c500ff8d73a2156c731a7664830c95
gilfoyle <gilfoyle@gmail.com>
2023-08-12 22:01:08 +0530

    new list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { faker } from "https://cdn.skypack.dev/@faker-js/faker";
 const Card = (props) => {
 	return (
 		<div>
@@ -7,21 +7,23 @@ const Card = (props) => {
 				href="#"
 				className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 			>
-				<div className="flex flex-row">
-					<div class="flex-shrink-0">
+				<div className="flex justify-between">
+					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						{props.data.id}
+					</h5>
+					<div className="flex-shrink-0">
 						<img
-							class="w-8 h-8 rounded-full"
-							src="/docs/images/people/profile-picture-1.jpg"
+							className="w-8 h-8 rounded-full"
+							src={faker.image.avatar()}
 							alt="Neil image"
 						/>
 					</div>
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-						{props.data.id}
-					</h5>
 				</div>
-				<p class="font-normal text-gray-700 dark:text-gray-400">
+				<p className="font-normal text-gray-700 dark:text-gray-400">
 					{props.data.title}
 				</p>
+				<p>{props.data.tag}</p>
+				<i class="fa-solid fa-triangle-exclamation"></i>
 			</a>
 		</div>
 	);
