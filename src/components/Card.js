191a5738fe78a8a3990e6dcbdeaff7f2434b4415
gilfoyle <gilfoyle@gmail.com>
2023-08-12 23:07:24 +0530

    new list

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,28 +3,23 @@ import { faker } from "https://cdn.skypack.dev/@faker-js/faker";
 const Card = (props) => {
 	return (
 		<div>
-			<a
-				href="#"
-				className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-			>
+			<div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
 				<div className="flex justify-between">
-					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
 						{props.data.id}
 					</h5>
 					<div className="flex-shrink-0">
 						<img
 							className="w-8 h-8 rounded-full"
 							src={faker.image.avatar()}
-							alt="Neil image"
+							alt="user"
 						/>
 					</div>
 				</div>
-				<p className="font-normal text-gray-700 dark:text-gray-400">
-					{props.data.title}
-				</p>
+				<p className="font-normal text-gray-700">{props.data.title}</p>
 				<p>{props.data.tag}</p>
 				<i class="fa-solid fa-triangle-exclamation"></i>
-			</a>
+			</div>
 		</div>
 	);
 };
--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,8 @@ import Card from "./Card";
 
 const Page = () => {
 	const [data, setData] = useState([]);
+	const [sortBy, setSortBy] = useState("title");
+	const [tics, setTics] = useState(data);
 	const apiUrl = "https://apimocha.com/quicksell/data";
 
 	useEffect(() => {
@@ -17,23 +19,51 @@ const Page = () => {
 				console.error("Error fetching data:", error);
 			});
 	}, []);
+
+	const sortTickets = (key) => {
+		const sortedTickets = [...tics.tickets].sort((a, b) => {
+			if (key === "title") {
+				return a.title.localeCompare(b.title);
+			}
+			if (key === "priority") {
+				return a.priority - b.priority;
+			}
+			return 0;
+		});
+
+		setTics(sortedTickets);
+		console.log(tics);
+	};
+
+	const handleSortChange = (event) => {
+		const selectedValue = event.target.value;
+		setSortBy(selectedValue);
+		sortTickets(selectedValue);
+	};
+
 	return (
 		<div>
+			<select
+				value={sortBy}
+				onChange={handleSortChange}
+				className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10"
+			>
+				<option value="title">Title</option>
+				<option value="priority">Priority</option>
+			</select>
 			{/* <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
 				<option>SM</option>
 				<option>M</option>
 				<option>L</option>
 				<option>XL</option>
-			</select>
-			<select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-				<option>SM</option>
-				<option>M</option>
-				<option>L</option>
-				<option>XL</option>
 			</select> */}
-			{/* <h1>{ticdata.id}</h1> */}
-			{console.log("hiii")}
-			{/* {console.log(ticdata)} */}
+
+			{tics.tickets?.map((ticket) => (
+				<div className="mb-3">
+					<Card data={ticket} />
+				</div>
+			))}
+
 			{/* {data.tickets?.length > 0 && (
 				<ul>
 					{data.tickets?.map((item) => (
@@ -41,11 +71,12 @@ const Page = () => {
 					))}
 				</ul>
 			)} */}
-			{data.tickets?.map((ticket) => (
+
+			{/* {data.tickets?.map((ticket) => (
 				<div className="mb-3">
 					<Card data={ticket} />
 				</div>
-			))}
+			))} */}
 		</div>
 	);
 };
